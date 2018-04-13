055eb1b8491a59b3220b68aef50b15c496f46f26
Alexander Suter <alexander.suter.ch@gmail.com>
2018-04-13 22:33:57 +0200

    refactor config

--- src/app/Config.php
+++ src/app/Config.php
@@ -11,36 +11,23 @@ class Config {
         define('CDN_HOST_SPRINT', CDN_HOST . '/sprint/');
         define('CDN_HOST_NIGHTLY', CDN_HOST . '/nightly/');
         
-        define('BASE_URL', self::getRequestedBaseUri());
-        define('PERMALINK_BASE', BASE_URL . '/permalink/ivy/');
-        define('PERMALINK_SPRINT', PERMALINK_BASE . 'sprint/');
-        define('PERMALINK_NIGHTLY', PERMALINK_BASE . 'nightly/');
-        define('PERMALINK_STABLE', PERMALINK_BASE . 'stable/');
-        
-        
-        
-        
-        
-        
-        
-        
+        $BASE_URL = self::getRequestedBaseUri();
+        $PERMALINK_BASE_URL = $BASE_URL . '/permalink/ivy/';
+        define('PERMALINK_SPRINT', $PERMALINK_BASE_URL . 'sprint/');
+        define('PERMALINK_NIGHTLY', $PERMALINK_BASE_URL . 'nightly/');
+        define('PERMALINK_STABLE', $PERMALINK_BASE_URL . 'stable/');
         
         define('MAVEN_SUPPORTED_RELEASES_SINCE_VERSION', '6.0.0');
- 
+        
         $rootDir = '/home/axonivya/www/developer.axonivy.com';
         if (self::isDevOrTestEnv()) {
             $rootDir = __DIR__ . '/../../test/data/webroot';
         }
-        
         define('IVY_RELEASE_DIRECTORY', StringUtil::createPath([$rootDir, 'releases', 'ivy']));
+        define('IVY_SPRINT_RELEASE_DIRECTORY', StringUtil::createPath([IVY_RELEASE_DIRECTORY, 'sprint']));
+        define('IVY_NIGHTLY_RELEASE_DIRECTORY', StringUtil::createPath([IVY_RELEASE_DIRECTORY, 'nightly']));
         
-        define('IVY_SPRINT_RELEASE_DIR_RELATIVE', 'sprint');
-        define('IVY_SPRINT_RELEASE_DIRECTORY', StringUtil::createPath([IVY_RELEASE_DIRECTORY, IVY_SPRINT_RELEASE_DIR_RELATIVE]));
-        
-        define('IVY_NIGHTLY_RELEASE_DIR_RELATIVE', 'nightly');
-        define('IVY_NIGHTLY_RELEASE_DIRECTORY', StringUtil::createPath([IVY_RELEASE_DIRECTORY, IVY_NIGHTLY_RELEASE_DIR_RELATIVE]));
-        
-        $UNSAVE_VERSIONS = [
+        define('UNSAFE_RELEASES', [
             '6.7.1' => '7.0',
             '6.7.0' => '7.0',
             '6.6.1' => '7.0',
@@ -61,8 +48,7 @@ class Config {
             '6.0.2' => '6.0.11',
             '6.0.1' => '6.0.11',
             '6.0.0' => '6.0.11',
-        ];
-        define('UNSAFE_RELEASES', $UNSAVE_VERSIONS);
+        ]);
     }
     
     private static function getRequestedBaseUri(): string
