1d72eea94d3372f8827ca2c8f33bc56e187ce1af
Alexander Suter <alexander.suter@axonivy.com>
2018-07-30 11:16:22 +0200

    Add docker example on sprint/nightly release page

--- src/app/release/SprintNightlyAction.php
+++ src/app/release/SprintNightlyAction.php
@@ -22,21 +22,25 @@ class SprintNightlyAction
         $urlVersion = '';
         $p2Url = '';
         $testingPurpose = true;
+        $dockerImage = '';
         if ($version == 'nightly') {
             $artifacts = ReleaseInfoRepository::getNightlyArtifacts();
             $name = 'Nightly Build';
             $urlVersion = 'nightly';
             $p2Url = 'https://file.axonivy.rocks/p2/nightly/';
+            $dockerImage = 'axonivydev/axonivy-engine:nightly';
         } else if ($version == 'sprint-release') {
             $artifacts = ReleaseInfoRepository::getSprintArtifacts();
             $name = 'Sprint Release';
             $urlVersion = 'sprint';
             $p2Url = 'https://file.axonivy.rocks/p2/sprint/';
+            $dockerImage = 'axonivydev/axonivy-engine:sprint';
         } else if ($version == 'stable') {
             $artifacts = ReleaseInfoRepository::getLatest()->getArtifacts();
             $name = 'Stable Release';
             $urlVersion = 'stable';
             $testingPurpose = false;
+            $dockerImage = 'axonivydev/axonivy-engine';
         } else {
             throw new \InvalidArgumentException($version . ' is not supported');
         }
@@ -47,7 +51,8 @@ class SprintNightlyAction
             'name' => $name,
             'currentUrl' => $baseUrl,
             'p2Url' => $p2Url,
-            'testingPurpose' => $testingPurpose
+            'testingPurpose' => $testingPurpose,
+            'dockerImage' => $dockerImage
         ]);
     }
     
