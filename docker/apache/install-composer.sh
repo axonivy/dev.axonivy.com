b70f44bebfd9c492a2d1138df1088f1b49b77acc
Alexander Suter <alexander.suter@axonivy.com>
2018-10-01 11:09:25 +0200

    Improve composer setup

--- docker/apache/install-composer.sh
+++ docker/apache/install-composer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 # https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
 EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
@@ -10,3 +12,7 @@ then
     rm composer-setup.php
     exit 1
 fi
+
+php composer-setup.php --version=1.6.3 --install-dir=/var
+rm composer-setup.php
+mv /var/composer.phar /usr/local/bin/composer
