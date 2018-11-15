7bbf62716e88436ab1d06e81e981e2a7d0fadedd
Alexander Suter <alexander.suter@axonivy.com>
2018-11-15 11:20:28 +0100

    Provide Demos and Workflow UIs

--- src/app/permalink/LibPermalink.php
+++ src/app/permalink/LibPermalink.php
@@ -5,26 +5,6 @@ use Slim\Exception\NotFoundException;
 
 class LibPermalink
 {
-    private function getMavenInfos($name): ?array {
-        $artifacts = [
-            // demos
-            'quick-start-tutorial' => ['ch.ivyteam.ivy.project.demo', 'QuickStartTutorial'],
-            'workflow-demos' => ['ch.ivyteam.ivy.project.demo', 'WorkflowDemos'],
-            'error-handling-demos' => ['ch.ivyteam.ivy.project.demo', 'ErrorHandlingDemos'],
-            'connectivity-demos' => ['ch.ivyteam.ivy.project.demo', 'ConnectivityDemos'],
-            'rule-engine-demos' => ['ch.ivyteam.ivy.project.demo', 'RuleEngineDemos'],
-            //'demos-app' => ['ch.ivyteam.ivy.project', 'IvyDemoApp'],
-            
-            // workflow ui
-            'jsf-workflow-ui' => ['ch.ivyteam.ivy.project.wf', 'JsfWorkflowUi'],
-            'html-workflow-ui' => ['ch.ivyteam.ivy.project.wf', 'HtmlWfUi'],
-
-            // full application zip demos
-            'demo-app' => ['ch.ivyteam.ivy.project.demo.ci.deploy.application', 'application'],
-        ];
-        return $artifacts[$name] ?? null;
-    }
-    
     public function __invoke($request, $response, $args) {
         $version = $args['version'];
         if ($version != 'dev') {
@@ -36,15 +16,12 @@ class LibPermalink
         $type = pathinfo($name, PATHINFO_EXTENSION); // e.g. zip
         $filename = pathinfo($name, PATHINFO_FILENAME); // e.g. demo-app
         
-        $mavenInfo = $this->getMavenInfos($filename);
-        if ($mavenInfo == null) {
+        $mavenArtifact = MavenArtifact::getMavenArtifact($filename);
+        if ($mavenArtifact == null) {
             throw new NotFoundException($request, $response);
         }
-        
-        $groupId = $mavenInfo[0];
-        $artifactId = $mavenInfo[1];
 
-        $url = $this->createLink($groupId, $artifactId, $type);
+        $url = $this->createLink($mavenArtifact->getGroupId(), $mavenArtifact->getArtifactId(), $type);
         
         return $response->withRedirect($url);
     }
---
+++ src/app/permalink/MavenArtifact.php
@@ -0,0 +1,89 @@
+<?php
+namespace app\permalink;
+
+class MavenArtifact
+{
+    //'demos-app' => ['ch.ivyteam.ivy.project', 'IvyDemoApp'],
+    
+    public static function getMavenArtifact($name): ?MavenArtifact {
+        $artifacts = self::getAll();
+        foreach ($artifacts as $artifact) {
+            if ($artifact->getName() == $name) {
+                return $artifact;
+            }
+        }
+        return null;        
+    }
+    
+    private static function getAll(): ?array
+    {
+        return array_merge(self::getWorkflowUis(), self::getProjectDemos(), self::getDemoApps());
+    }
+    
+    public static function getProjectDemos(): ?array
+    {
+        $groupdId = 'ch.ivyteam.ivy.project.demo';
+        return [
+            new MavenArtifact('quick-start-tutorial', $groupdId, 'QuickStartTutorial', 'iar'),
+            new MavenArtifact('workflow-demos', $groupdId, 'WorkflowDemos', 'iar'),
+            new MavenArtifact('error-handling-demos', $groupdId, 'ErrorHandlingDemos', 'iar'),
+            new MavenArtifact('connectivity-demos', $groupdId, 'ConnectivityDemos', 'iar'),
+            new MavenArtifact('rule-engine-demos', $groupdId, 'RuleEngineDemos', 'iar')
+        ];
+    }
+
+    public static function getWorkflowUis(): ?array
+    {
+        $groupdId = 'ch.ivyteam.ivy.project.wf';
+        return [
+            new MavenArtifact('jsf-workflow-ui', $groupdId, 'JsfWorkflowUi', 'iar'),
+            new MavenArtifact('html-workflow-ui', $groupdId, 'HtmlWfUi', 'iar'),
+        ];
+    }
+    
+    private static function getDemoApps(): ?array
+    {
+        return [
+            new MavenArtifact('demo-app', 'ch.ivyteam.ivy.project.demo.ci.deploy.application', 'application', 'zip'),
+        ];
+    }
+    
+    private $name;
+    
+    private $groupId;
+    private $artifactId;
+    private $type;
+    
+    public function __construct($name, $groupId, $artifactId, $type)
+    {
+        $this->name = $name;
+        $this->groupId = $groupId;
+        $this->artifactId = $artifactId;
+        $this->type = $type;
+    }
+    
+    private function getName(): string
+    {
+        return $this->name;
+    }
+    
+    public function getGroupId(): string
+    {
+       return $this->groupId;
+    }
+    
+    public function getArtifactId(): string
+    {
+        return $this->artifactId;
+    }
+    
+    public function getDisplayName(): string
+    {
+        return ucwords(str_replace('-', ' ', $this->name));
+    }
+    
+    public function getPermalinkDev()
+    {
+        return '/permalink/lib/dev/' . $this->name . '.' . $this->type;
+    }
+}
