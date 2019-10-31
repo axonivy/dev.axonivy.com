9406f5d1393e3cea0f0070a174f6508687472641
Alexander Suter <alexander.suter@axonivy.com>
2019-10-31 14:25:11 +0100

    Implement caching of http request on request scope

--- src/app/permalink/MavenArtifact.php
+++ src/app/permalink/MavenArtifact.php
@@ -3,6 +3,173 @@ namespace app\permalink;
 
 use app\util\StringUtil;
 
+class MavenArtifact
+{
+    private $key;
+
+    private $name;
+
+    private $groupId;
+
+    private $artifactId;
+
+    private $type;
+
+    private $versionCache = null;
+
+    private $makesSenseAsMavenDependency;
+
+    private $isDocumentation;
+
+    function __construct($key, $name, $groupId, $artifactId, $type, bool $makesSenseAsMavenDependency, bool $isDocumentation)
+    {
+        $this->key = $key;
+        $this->name = $name;
+        $this->groupId = $groupId;
+        $this->artifactId = $artifactId;
+        $this->type = $type;
+        $this->makesSenseAsMavenDependency = $makesSenseAsMavenDependency;
+        $this->isDocumentation = $isDocumentation;
+    }
+    
+    public static function create(string $key): MavenArtifactBuilder
+    {
+        return new MavenArtifactBuilder($key);
+    }
+
+    private function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function getGroupId(): string
+    {
+        return $this->groupId;
+    }
+
+    public function getArtifactId(): string
+    {
+        return $this->artifactId;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getDisplayName(): string
+    {
+        return $this->name;
+    }
+
+    public function getMakesSenseAsMavenDependency(): bool
+    {
+        return $this->makesSenseAsMavenDependency;
+    }
+
+    public function isDocumentation(): bool
+    {
+        return $this->isDocumentation;
+    }
+
+    public function getDocUrl($version)
+    {
+        return $this->getUrl($version) . '!/index.html';
+    }
+    
+    public function getDevUrl()
+    {
+        $versions = $this->getVersions();
+        if (empty($versions)) {
+            return "";
+        }
+        return $this->getUrl($versions[0]);
+    }
+
+    public function getUrl($version)
+    {
+        $concretVersion = $version;
+
+        $baseUrl = $this->getBaseUrl();
+        if (StringUtil::contains($version, 'SNAPSHOT')) {
+            $xml = HttpRequester::request("$baseUrl/$version/maven-metadata.xml");
+            if (empty($xml)) {
+                return "";
+            }
+            $concretVersion = self::parseVersionIdentifierFromXml($xml);
+        }
+        return $baseUrl . '/' . $version . '/' . $this->artifactId . '-' . $concretVersion . '.' . $this->type;
+    }
+
+    public static function parseVersionIdentifierFromXml(string $xml): string
+    {
+        $element = new \SimpleXMLElement($xml);
+        $result = $element->xpath('/metadata/versioning/snapshotVersions/snapshotVersion');
+        return $result[0]->value;
+    }
+
+    private function getBaseUrl()
+    {
+        $groupId = str_replace('.', '/', $this->groupId);
+        return MAVEN_ARTIFACTORY_URL . "$groupId/" . $this->artifactId;
+    }
+
+    public function getPermalinkDev()
+    {
+        return '/permalink/lib/dev/' . $this->key . '.' . $this->type;
+    }
+
+    public function getVersions(): array
+    {
+        if ($this->versionCache == null) {
+            $baseUrl = $this->getBaseUrl();
+            
+            $xml = HttpRequester::request("$baseUrl/maven-metadata.xml");
+
+            if (empty($xml)) {
+                $this->versionCache = [];
+                return $this->versionCache;
+            }
+
+            $v = self::parseVersions($xml);
+            
+            usort($v, 'version_compare');
+            $v = array_reverse($v);
+
+            $this->versionCache = $v;
+        }
+        return $this->versionCache;
+    }
+    
+    public static function parseVersions($xml) {
+        $element = new \SimpleXMLElement($xml);
+        $result = $element->xpath('/metadata/versioning/versions');
+        $versions = get_object_vars($result[0]->version);
+        return array_values($versions);
+    }
+}
+
+/* caching on request scope */
+class HttpRequester
+{
+    private static $cache = [];
+    
+    static function request($url)
+    {
+        if (!isset(self::$cache[$url]))
+        {
+            $headers = get_headers($url);
+            $statusCode = substr($headers[0], 9, 3);
+            $content = '';
+            if ($statusCode == "200") {
+                $content = file_get_contents($url);
+            }
+            self::$cache[$url] = $content;
+        }
+        return self::$cache[$url];
+    }
+}
+
 class MavenArtifactBuilder
 {
     private $key;
@@ -67,159 +234,3 @@ class MavenArtifactBuilder
     }
 }
 
-class MavenArtifact
-{
-    private $key;
-
-    private $name;
-
-    private $groupId;
-
-    private $artifactId;
-
-    private $type;
-
-    private $versionCache = null;
-
-    private $makesSenseAsMavenDependency;
-
-    private $isDocumentation;
-
-    public function __construct($key, $name, $groupId, $artifactId, $type, bool $makesSenseAsMavenDependency, bool $isDocumentation)
-    {
-        $this->key = $key;
-        $this->name = $name;
-        $this->groupId = $groupId;
-        $this->artifactId = $artifactId;
-        $this->type = $type;
-        $this->makesSenseAsMavenDependency = $makesSenseAsMavenDependency;
-        $this->isDocumentation = $isDocumentation;
-    }
-    
-    public static function create(string $key): MavenArtifactBuilder
-    {
-        return new MavenArtifactBuilder($key);
-    }
-
-    private function getKey(): string
-    {
-        return $this->key;
-    }
-
-    public function getGroupId(): string
-    {
-        return $this->groupId;
-    }
-
-    public function getArtifactId(): string
-    {
-        return $this->artifactId;
-    }
-
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    public function getDisplayName(): string
-    {
-        return $this->name;
-    }
-
-    public function getMakesSenseAsMavenDependency(): bool
-    {
-        return $this->makesSenseAsMavenDependency;
-    }
-
-    public function isDocumentation(): bool
-    {
-        return $this->isDocumentation;
-    }
-
-    public function getDocUrl($version)
-    {
-        return $this->getUrl($version) . '!/index.html';
-    }
-    
-    public function getDevUrl()
-    {
-        $versions = $this->getVersions();
-        if (!empty($versions)) {
-            return $this->getUrl($versions[0]);
-        }
-        return "";
-    }
-
-    public function getUrl($version)
-    {
-        $concretVersion = $version;
-
-        $baseUrl = $this->getBaseUrl();
-        if (StringUtil::contains($version, 'SNAPSHOT')) {
-            // TODO Cache
-            $xml = $this->get_contents("$baseUrl/$version/maven-metadata.xml");
-            if (empty($xml)) {
-                return "";
-            }
-            $concretVersion = self::parseVersionIdentifierFromXml($xml);
-        }
-        return $baseUrl . '/' . $version . '/' . $this->artifactId . '-' . $concretVersion . '.' . $this->type;
-    }
-
-    public static function parseVersionIdentifierFromXml(string $xml): string
-    {
-        $element = new \SimpleXMLElement($xml);
-        $result = $element->xpath('/metadata/versioning/snapshotVersions/snapshotVersion');
-        return $result[0]->value;
-    }
-
-    private function getBaseUrl()
-    {
-        $groupId = str_replace('.', '/', $this->groupId);
-        return MAVEN_ARTIFACTORY_URL . "$groupId/" . $this->artifactId;
-    }
-
-    public function getPermalinkDev()
-    {
-        return '/permalink/lib/dev/' . $this->key . '.' . $this->type;
-    }
-
-    public function getVersions(): array
-    {
-        if ($this->versionCache == null) {
-            $baseUrl = $this->getBaseUrl();
-            
-            $xml = $this->get_contents("$baseUrl/maven-metadata.xml");
-
-            if (empty($xml)) {
-                $this->versionCache = [];
-                return $this->versionCache;
-            }
-
-            $v = self::parseVersions($xml);
-            
-            usort($v, 'version_compare');
-            $v = array_reverse($v);
-
-            $this->versionCache = $v;
-        }
-        return $this->versionCache;
-    }
-    
-    public static function parseVersions($xml) {
-        $element = new \SimpleXMLElement($xml);
-        $result = $element->xpath('/metadata/versioning/versions');
-        $versions = get_object_vars($result[0]->version);
-        return array_values($versions);
-    }
-
-    private function get_contents($url)
-    {
-        $headers = get_headers($url);
-        $statusCode = substr($headers[0], 9, 3);
-        if ($statusCode == "200") {
-            return file_get_contents($url);
-        }
-        return "";
-    }
-}
