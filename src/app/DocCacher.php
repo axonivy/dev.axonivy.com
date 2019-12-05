ed50c0068177e84dfb4b1f0bbf06ede065568033
Alexander Suter <alexander.suter@axonivy.com>
2019-12-05 11:42:11 +0100

    Improve doccacher

--- src/app/DocCacher.php
+++ src/app/DocCacher.php
@@ -10,7 +10,7 @@ foreach (MavenArtifactRepository::getDocs() as $doc) {
     foreach ($doc->getVersions() as $version) {
         $targetDir = DOC_DIRECTORY_THIRDPARTY . '/' . $doc->getDocSubFolder($version);
         if (!file_exists($targetDir)) {
-            $cmd = CLONE_DOC_SCRIPT. ' ' . $doc->getUrl($version) . ' ' . $targetDir;
+            $cmd = CLONE_DOC_SCRIPT . ' ' . $doc->getUrl($version) . ' ' . $targetDir;
             echo "Execute: " . $cmd . "\n";
             exec($cmd);
         } else {
