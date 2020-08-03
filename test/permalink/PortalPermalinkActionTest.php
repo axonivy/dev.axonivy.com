37878921fd87ac37bc445dd93e838d33f1580e75
Alexander Suter <alexander.suter@axonivy.com>
2020-08-03 11:15:29 +0200

    Fix portal redirect tests

--- test/permalink/PortalPermalinkActionTest.php
+++ test/permalink/PortalPermalinkActionTest.php
@@ -43,25 +43,36 @@ class PortalPermalinkActionTest extends TestCase
     public function testPortalDoc()
     {
         AppTester::assertThatGet('/portal/8.0.3/doc')->redirect('/documentation/portal-guide/8.0.3');
-        AppTester::assertThatGet('/portal/8.0/doc')->redirect('/documentation/portal-guide/' . Market::getProductByKey('portal')->getLatestVersionToDisplay());
+        AppTester::assertThatGet('/portal/8.0/doc')->redirect('/documentation/portal-guide/' . self::latestVersionOfMinorVersionPortal('8.0'));
+    }
+
+    private static function latestVersionOfMinorVersionPortal($minorVersion)
+    {
+        $portal = Market::getProductByKey('portal');
+        $portalVersions = $portal->getVersionsToDisplay();
+        foreach ($portalVersions as $v) {
+            if (StringUtil::startsWith($v, $minorVersion)) {
+                return $v;
+            }
+        }     
     }
 
     public function testPortalDocWithDocument()
     {
         AppTester::assertThatGet('/portal/8.0.3/doc/test.html')->redirect('/documentation/portal-guide/8.0.3/test.html');
-        AppTester::assertThatGet('/portal/8.0/doc/test.html')->redirect('/documentation/portal-guide/' . Market::getProductByKey('portal')->getLatestVersionToDisplay() . '/test.html');
+        AppTester::assertThatGet('/portal/8.0/doc/test.html')->redirect('/documentation/portal-guide/' . self::latestVersionOfMinorVersionPortal('8.0') . '/test.html');
     }
 
     public function testPortalDocWithDocumentInSubfolder()
     {
         AppTester::assertThatGet('/portal/8.0.3/doc/subfolder/test.html')->redirect('/documentation/portal-guide/8.0.3/subfolder/test.html');
-        AppTester::assertThatGet('/portal/8.0/doc/subfolder/test.html')->redirect('/documentation/portal-guide/' . Market::getProductByKey('portal')->getLatestVersionToDisplay() . '/subfolder/test.html');
+        AppTester::assertThatGet('/portal/8.0/doc/subfolder/test.html')->redirect('/documentation/portal-guide/' . self::latestVersionOfMinorVersionPortal('8.0') . '/subfolder/test.html');
     }
 
     public function testPortalBrokenLink()
     {
         AppTester::assertThatGet('/portal/8.0/doc/portal-developer-guide/introduction/index.html#new-and-noteworthy')
-            ->redirect('/documentation/portal-guide/' . Market::getProductByKey('portal')->getLatestVersionToDisplay() . '/portal-developer-guide/introduction/index.html');
+            ->redirect('/documentation/portal-guide/' . self::latestVersionOfMinorVersionPortal('8.0') . '/portal-developer-guide/introduction/index.html');
     }
 
     private function getLatestVersion($version)
