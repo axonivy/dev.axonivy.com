a806d1c94ff3f2622174dc85a785305ef15eb12a
Alexander Suter <alexander.suter@axonivy.com>
2021-12-10 09:43:41 +0100

    By default look for an openapi.json in product directory

--- src/app/domain/market/OpenAPIProvider.php
+++ src/app/domain/market/OpenAPIProvider.php
@@ -19,8 +19,8 @@ class OpenAPIProvider
 
     if (filter_var($url, FILTER_VALIDATE_URL)) {
       return urlencode($url);
-    } else if (! empty($url) && file_exists($this->product->getProductFile($version, $url))) {
-      return $this->product->assetBaseUrl($version) . "/openapi";
+    } else if (!empty($url) && file_exists($this->product->getProductFile($version, $url))) {
+      return $this->product->assetBaseUrl($version) . "/$url";
     }
     return "";
   }
@@ -40,6 +40,14 @@ class OpenAPIProvider
   
   private function openApiUrl(string $version): string
   {
+    // auto detect openapi.json
+    $defaultFile = 'openapi.json';
+    $openApiJson = $this->getProductFile($version, $defaultFile);
+    if (file_exists($openApiJson)) {
+      return $defaultFile;
+    }
+
+    // read from product.json
     $content = $this->product->getProductJsonContent($version);
     $json = json_decode($content);
     return $json->properties->openApiUrl ?? '';
