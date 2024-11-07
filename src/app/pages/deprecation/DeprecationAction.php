7b7d50bf545789ece0a63cd870b4408ce7dc950c
Lukas Lieb <lukas.lieb@axonivy.com>
2024-11-07 14:03:35 +0100

    Update deprecation features list

--- src/app/pages/deprecation/DeprecationAction.php
+++ src/app/pages/deprecation/DeprecationAction.php
@@ -31,7 +31,7 @@ class DeprecationAction
   
   private static function getVersions(): array
   {
-    return ["3", "4", "5", "6", "7", "8", "10", "11.1", "11.2"];
+    return ["3", "4", "5", "6", "7", "8", "10", "12"];
   }
   
   private static function getFeatures(): array
