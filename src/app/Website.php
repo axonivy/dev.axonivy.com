94026b1d244118680cacd2edb0d97c0fd4a51d0f
Alexander Suter <alexander.suter@axonivy.com>
2026-08-28 15:41:56 +0200

    Fix template engine

--- src/app/Website.php
+++ src/app/Website.php
@@ -23,7 +23,6 @@ class Website
   {
     $container = $this->createDiContainer();
     $this->app = AppFactory::createFromContainer($container);
-    $this->configureTemplateEngine();
     $this->installTrailingSlashRedirect();
     $this->installRoutes();
     $this->installErrorHandling();
@@ -31,10 +30,7 @@ class Website
 
   private function createDiContainer(): Container
   {
-    $builder = new ContainerBuilder();
-    $builder->addDefinitions([
-      Twig::class => Twig::create(__DIR__ . '/../app/pages')
-    ]);
+    $builder = new ContainerBuilder();    
     return $builder->build();
   }
 
@@ -46,33 +42,6 @@ class Website
   public function start()
   {
     $this->app->run();
-  }
-
-  private function configureTemplateEngine(): Twig
-  {
-    $container = $this->app->getContainer();
-    $view = $container->get(Twig::class);
-
-    $releaseTypeLTS = ReleaseType::LTS();
-    $releaseTypeLE = ReleaseType::LE();
-
-    $versionLTS = $this->getDisplayVersion($releaseTypeLTS->releaseInfo());
-    $leRelease = $this->getDisplayVersion($releaseTypeLE->releaseInfo());
-
-    $text = $versionLTS;
-    $textLong = $releaseTypeLTS->shortName() . " $versionLTS";
-    if (!empty($leRelease)) {
-      $text .= " / $leRelease";
-      $textLong .= " / " . $releaseTypeLE->shortName() . " $leRelease";
-    }
-    $view->getEnvironment()->addGlobal('CURRENT_VERSION_DOWNLOAD', $text);
-    $view->getEnvironment()->addGlobal('CURRENT_VERSION_DOWNLOAD_LONG', $textLong);
-
-    $view->getEnvironment()->addGlobal('PRODUCTIVE_SYSTEM', Config::isProductionEnvironment());
-
-    $view->getEnvironment()->addGlobal('BASE_URL', $this->baseUrl());
-
-    return $view;
   }
 
   private function baseUrl()
