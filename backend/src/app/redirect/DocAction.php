396afc486399fcca69923d066b7b62c4641f6d96
Alexander Suter <alexander.suter@axonivy.com>
2026-09-17 09:38:08 +0200

    Fix tests

--- backend/src/app/redirect/DocAction.php
+++ backend/src/app/redirect/DocAction.php
@@ -39,12 +39,6 @@ class DocAction
       return Redirect::to($response, $url . $docPath);
     }
 
-    // archived docs
-    $archivedDocs = ["9.4", "9.3", "9.2", "9.1", "7.4", "7.3", "7.2", "7.1", "6.7", "6.6", "6.5", "6.4", "6.3", "6.2", "6.1", "6.0", "5.1", "5.0", "3.9"];
-    if (in_array($version, $archivedDocs)) {
-      return Redirect::to($response, "https://archive.axonivy.com/doc/" . $version . '/en' . $docPath);
-    }
-
     // nightly-8.0
     if (str_starts_with($version, "nightly-")) {
       $v = str_replace("nightly-", "", $version);
@@ -70,6 +64,11 @@ class DocAction
     $version = $v->getMinorVersion();
     $docProvider = new DocProvider($version);    
     if (!$docProvider->exists()) {
+      // archived docs
+      $archivedDocs = ["9.4", "9.3", "9.2", "9.1", "7.4", "7.3", "7.2", "7.1", "6.7", "6.6", "6.5", "6.4", "6.3", "6.2", "6.1", "6.0", "5.1", "5.0", "3.9"];
+      if (in_array($version, $archivedDocs)) {
+        return Redirect::to($response, "https://archive.axonivy.com/doc/" . $version . '/en' . $docPath);
+      }
       throw new HttpNotFoundException($request);
     }
 
